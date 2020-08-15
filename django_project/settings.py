016891a7c8b97749568599870fd76ce3e597f939
TingKai911 <tingkai911@gmail.com>
2020-08-15 20:01:25 +0800

    update static_dir

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static/blog/'),
+    os.path.join(BASE_DIR, 'static/css/'),
+    os.path.join(BASE_DIR, 'static/js')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
