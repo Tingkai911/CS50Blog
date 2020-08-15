01e156515730f3acb6b44900e504bc0112ffce38
TingKai911 <tingkai911@gmail.com>
2020-08-15 19:09:16 +0800

    more changes in settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static/blog/'),
+    os.path.join(BASE_DIR, 'static/css/'),
+    os.path.join(BASE_DIR, 'static/js'),
+    os.path.join(BASE_DIR, 'node_moudles/')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
